70becc4561e60e76c407b7548e6dc66a89339083
madhu <msn.edu94@gmail.com>
2026-08-06 18:21:51 +0100

    replacing all with actual repo link

--- scripts/set-repo-url.sh
+++ scripts/set-repo-url.sh
@@ -10,6 +10,6 @@ repo_url="$1"
 repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 find "$repo_root" -type f \( -name '*.yaml' -o -name '*.yml' \) -print0 \
-  | xargs -0 sed -i "s#https://github.com/YOUR_ORG/multi-cluster-multi-apps-poc.git#${repo_url}#g"
+  | xargs -0 sed -i "s#https://github.com/msnops/multi-cluster-multi-apps-poc.git#${repo_url}#g"
 
 echo "Repository URL updated to: $repo_url"
